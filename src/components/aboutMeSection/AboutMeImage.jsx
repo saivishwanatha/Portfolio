357645c96fcd22cf8320664dc0866b26fa08eb10
Sai Charan <saicharan.vish@gmail.com>
2025-05-11 01:54:29 -0500

    commits

--- src/components/aboutMeSection/AboutMeImage.jsx
+++ src/components/aboutMeSection/AboutMeImage.jsx
@@ -1,10 +1,10 @@
-
+import aboutMeImage from "/images/about-me.jpg";
 const AboutMeImage = () => {
   return (
     <div className="h-[500px] w-[300px] relative">
       <div className="h-[500px] w-[300px] rounded-[100px] absolute overflow-hidden">
         <img
-          src="../../../public/images/about-me.jpg"
+          src={aboutMeImage}
           alt="About Me Image"
           className="h-full w-auto object-cover"
         />
